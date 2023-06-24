85c25e84dcf81565de256dd21af1de6bb3307702
Sakib Rahman <85602964+thesakibrahman@users.noreply.github.com>
2023-06-24 12:36:58 +0530

    Add files via upload

---
+++ Source Code/js/script.js
@@ -0,0 +1,64 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   profile.classList.remove('active');
+}
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   searchForm.classList.remove('active');
+   navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   profile.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active');
+}
+
+document.querySelectorAll('.content-150').forEach(content => {
+   if(content.innerHTML.length > 150) content.innerHTML = content.innerHTML.slice(0, 150);
+});
+
+//ading part
+
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
